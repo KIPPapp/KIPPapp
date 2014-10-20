105cb9838f5dced9cf9409842f1cd856259b1ef6
mgorkani <mgorkani@gmail.com>
2014-10-19 20:56:31 -0700

    added groups

--- GroupCell.swift
+++ GroupCell.swift
@@ -9,27 +9,32 @@
 import UIKit
 
 class GroupCell: UITableViewCell {
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        self.layer.borderWidth = 1.5
+        self.layer.cornerRadius = 2.0
+        self.layer.borderColor = UIColor.grayColor().CGColor
+        self.backgroundColor = UIColor(red: CGFloat(Float(arc4random()) / Float(UINT32_MAX)), green: CGFloat(Float(arc4random()) / Float(UINT32_MAX)), blue: CGFloat(Float(arc4random()) / Float(UINT32_MAX)), alpha: CGFloat(Float(arc4random()) / Float(UINT32_MAX)))
     }
     
     var group:String? = nil {
         didSet {
             groupName.text = group!
-           
+            
             
         }
         
     }
-
-
+    
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     @IBOutlet weak var groupName: UILabel!
-
+    
 }
--- GroupController.swift
+++ GroupController.swift
@@ -9,15 +9,28 @@
 import UIKit
 
 class GroupController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
     var groups:[String] = []
     var students:[Student] = []
-   
+    var studentList:[Student] = []
+    
+    let headers =
+    ["Operations and Algebraic Thinking",
+        "Complex Number Systems",
+        "Geometry",
+        "Statistics and probability"] as NSArray
+    
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.dataSource = self
         tableView.delegate = self
+        
         self.view.showActivityViewWithLabel("Loading")
+        
+        //Get topics
         var parseAPI:ParseAPI = (self.tabBarController as KippAppController).parseAPI
         parseAPI.getStudentData("Mia Hamm", grade:"Secondary Intervention") { (students, groups, error) -> () in
             for group in groups! {
@@ -27,21 +40,23 @@ class GroupController: UIViewController, UITableViewDataSource, UITableViewDeleg
             self.students = students!
             self.view.hideActivityView()
             self.tableView.reloadData()
-            
         }
-
-
+        
+        
         // Do any additional setup after loading the view.
     }
-
-    @IBOutlet weak var tableView: UITableView!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return headers.count
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return groups.count
+        return groups.count/headers.count
     }
     
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
@@ -53,8 +68,26 @@ class GroupController: UIViewController, UITableViewDataSource, UITableViewDeleg
         return cell
     }
     
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var headerLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        headerLabel.text = self.headers[section] as? String
+        
+        var headerText = self.headers[section] as NSString
+        return headerText
+    }
+    /*
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    var headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 320))
+    headerView.backgroundColor = UIColor(red: 14.0/255.0, green: 114.0/255.0, blue: 199.0/255.0, alpha: 0.75)
+    var headerLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+    headerLabel.text = self.headers[section] as? String
+    
+    headerView.addSubview(headerLabel)
+    return headerView
+    }*/
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if (segue.identifier == "showMembers") {
+  /*      if (segue.identifier == "showMembers") {
             
             var indexPath:NSIndexPath = self.tableView.indexPathForSelectedRow()!
             let groupName = groups[indexPath.row]
@@ -65,25 +98,25 @@ class GroupController: UIViewController, UITableViewDataSource, UITableViewDeleg
                     studentList.append(student)
                 }
             }
-         
+            
             let navigationController = segue.destinationViewController as UINavigationController
             let detailViewController = navigationController.viewControllers[0] as StudentMasterViewController
             detailViewController.studentList = studentList
             
-        }
+        } */
         
     }
-
     
-
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }

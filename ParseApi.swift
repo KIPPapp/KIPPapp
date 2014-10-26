22e2db75c5edcaeac5f145c593c18cfe20ac318b
mgorkani <mgorkani@gmail.com>
2014-10-26 16:13:07 -0700

    fixed nsnumber conversion

--- ParseApi.swift
+++ ParseApi.swift
@@ -39,7 +39,7 @@ class ParseAPI {
                     // The find succeeded.
                     NSLog("Successfully retrieved \(objects.count) Student Stats.")
                     // Do something with the found objects
-                    var index:NSNumber = 1
+                    var index:Int = 1
                     for object in objects {
                    
                         var student:Student = Student()
@@ -53,7 +53,7 @@ class ParseAPI {
                         student.grade = object["Grade"] as String
                         student.teacher = object["Teacher"] as String
                         student.currentNumTries = (object["currentNumTries"] as String).toInt()!
-                        student.imagePath = "\(index.stringValue).png"
+                        student.imagePath = "\(index.description).png"
                         index=index+1
                         student.minLastWeek = (object["minLastWeek"] as String).toInt()!
                         self.students.append(student)
